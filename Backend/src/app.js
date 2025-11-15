ee8913631cee59e6294b940e5b7a5c2d368cc5fa
Biswarup Biswas <117425933+biswa2005@users.noreply.github.com>
2025-11-16 01:17:02 +0530

    hi

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URI, credentials: true }));
 
 app.use("/api/v1/auth", authRoutes);
 
